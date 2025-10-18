9c43ff64d64e2b8cce618394bddb6bcbcb344ed5
jumontenegrol <jumontenegrol@unal.edu.co>
2025-10-18 15:55:36 -0500

    mucho mejor

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ fetch('productos.json')
         <div class="bg-white shadow-md rounded-xl overflow-hidden hover:scale-105 transition-transform duration-200">
           <img src="${p.imagen}" alt="${p.nombre}" class="w-full h-48 object-cover" onerror="this.onerror=null;this.src='https://via.placeholder.com/400x300?text=Imagen+no+disponible'">
           <div class="p-4">
-            <h3 class="text-lg font-semibold text-pink-700">${p.nombre}</h3>
+            <h3 class="text-lg font-semibold text-red-700">${p.nombre}</h3>
             <p class="text-sm text-gray-600 mb-2">${p.descripcion}</p>
             <p class="font-bold text-gray-800 mb-3">$${p.precio}</p>
-            <a href="https://wa.me/573001234567?text=Hola!%20Estoy%20interesado%20en%20el%20producto%20${encodeURIComponent(p.nombre)}"
+            <a href="https://wa.me/573005554942?text=Hola!%20Estoy%20interesado%20en%20el%20producto%20${encodeURIComponent(p.nombre)}"
               target="_blank"
-              class="block bg-pink-400 text-white text-center py-2 rounded-md hover:bg-pink-500">
+              class="block bg-red-800 text-white text-center py-2 rounded-md hover:bg-red-200">
               Comprar por WhatsApp
             </a>
           </div>
